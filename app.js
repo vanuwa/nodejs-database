137e120800d0ef6383b464b214e13271ed3ea998
Va Nuwa <vanuwa@gmail.com>
2017-05-08 21:14:06 +0300

    implemented create action of custom database file module;

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
  */
 const Path = require('path');
 const fs = require('fs');
+const crypto = require('crypto');
+const util = require('util');
+
 const DB_PATH = 'storage';
 const DB_FILE_NAME = 'db.dat';
 
@@ -25,14 +28,89 @@ class DB {
     }
   }
 
-  create (record) {
-    // sure file exits
+  create (value) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this.file_path, 'utf8', (err, content) => {
+        if (err && err.code !== 'ENOENT') {
+          return reject(err);
+        }
 
-    // read content
+        const id = this._generateId(value);
+        const record = Object.assign({}, value, { id });
 
-    // append record
+        if (err && err.code === 'ENOENT') {
+          return this._write(JSON.stringify(record))
+            .then(resolve)
+            .catch(reject);
+        }
 
-    // write content
+        const db_obj = this._fromStringToObj(content);
+
+        if (db_obj[record.id]) {
+          const error = new Error(`Record ${util.inspect(record)} already exist.`);
+
+          return reject(error);
+        }
+
+        db_obj[record.id] = record;
+
+        const combined = this._fromObjToString(db_obj);
+
+        return this._write(combined)
+          .then(resolve)
+          .catch(reject);
+      });
+    });
+  }
+
+  _generateId (obj) {
+    const hash = crypto.createHash('sha256');
+    const salt = `${obj.name}|${obj.phone}`;
+
+    // return crypto.randomBytes(16).toString("hex");
+    return hash.update(salt).digest('hex');
+  }
+
+  _fromStringToObj (content) {
+    return content.toString().split('\n').reduce((result, row) => {
+      let json = null;
+
+      try {
+        if (row) {
+          json = JSON.parse(row);
+        }
+      } catch (exception) {
+        console.log('[ Oops ]', exception);
+      }
+
+      if (json) {
+        result[json.id] = json;
+      }
+
+      return result;
+    }, {});
+  }
+
+  _fromObjToString (obj) {
+    let content = '';
+
+    Object.keys(obj).forEach((key) => {
+      content = [content, JSON.stringify(obj[key])].join('\n')
+    });
+
+    return content;
+  }
+
+  _write (content) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(this.file_path, content, 'utf8', (error) => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve(content);
+      });
+    });
   }
 
   update (id, record) {
@@ -82,7 +160,7 @@ class DB {
       // sure file exists
 
       // read content
-      fs.readFile(this.file_path, 'UTF-8', (err, content) => {
+      fs.readFile(this.file_path, 'utf8', (err, content) => {
         if (err) {
           if (err.code === 'ENOENT') {
             return resolve('');
@@ -106,4 +184,12 @@ instance.list()
   })
   .catch((exception) => {
     console.log('[ LIST ][ EXCEPTION ]', exception);
+  });
+
+instance.create({ name: 'Vasil', phone: '023456' })
+  .then((result) => {
+    console.log('[ CREATE ] ', result);
+  })
+  .catch((exception) => {
+    console.log('[ CREATE ][ EXCEPTION ]', exception);
   });
